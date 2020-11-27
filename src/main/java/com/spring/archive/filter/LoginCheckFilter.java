ff07af1c0c3949a0d7c58916dac714f1b2536579
KimJeongho <kimjeongho@archive-asia.co.jp>
2020-11-27 14:56:59 +0900

    필터로 글쓰기, 글삭제, 글수정 로그인 체크 처리

---
+++ src/main/java/com/spring/archive/filter/LoginCheckFilter.java
@@ -0,0 +1,50 @@
+package com.spring.archive.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@WebFilter(value = {"/writing", "/boardDelete", "/boardModify"})
+public class LoginCheckFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest)request;
+		HttpSession session = httpRequest.getSession(false);
+		boolean loginChack = false;
+		if(session != null) {
+			if(session.getAttribute("USER") != null) {
+				loginChack = true;
+			}
+		}
+		if(loginChack) {
+			chain.doFilter(httpRequest, response);
+		} else {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/signin");
+			dispatcher.forward(request, response);
+		}
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
