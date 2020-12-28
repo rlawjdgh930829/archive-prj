e3eeba60c98967d33755a9312a4a8f56c61a65e1
KimJeongho <kimjeongho@archive-asia.co.jp>
2020-12-28 20:33:31 +0900

    memberService 생성

--- src/main/java/com/spring/archive/controller/MemberController.java
+++ src/main/java/com/spring/archive/controller/MemberController.java
@@ -15,12 +15,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.spring.archive.domain.LoginUserDTO;
 import com.spring.archive.domain.MemberDTO;
 import com.spring.archive.repository.MemberDAO;
+import com.spring.archive.service.MemberService;
 
 @Controller
 public class MemberController {
 	
 	@Autowired
-	private MemberDAO memberDAO;
+	private MemberService memberService;
 	
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
 	public String singupView(Model model) {
@@ -34,12 +35,7 @@ public class MemberController {
 		if(bindingResult.hasErrors()) {
 			returnValue = "index.jsp?page=body/signup";
 		} else {
-			Integer memberMaxNo = memberDAO.getMemeberMaxNo();
-			if(memberMaxNo == null) {
-				memberMaxNo = 0;
-			}
-			member.setMemberNo(memberMaxNo + 1);
-			memberDAO.insertMemeber(member);
+			memberService.memberSingupService(member);
 			returnValue = "redirect:/";
 		}
 		return returnValue;
@@ -57,7 +53,7 @@ public class MemberController {
 		if(bindingResult.hasErrors()) {
 			returnValue = "index.jsp?page=body/signin";
 		} else {
-			MemberDTO selectMemberResult = memberDAO.selectMember(user);
+			MemberDTO selectMemberResult = memberService.selectMemberService(user);
 			if(selectMemberResult == null) {
 				returnValue = "redirect:/signin";
 			} else {
@@ -72,7 +68,7 @@ public class MemberController {
 	@ResponseBody
 	public Boolean idCheck(@RequestParam String userId) {
 		boolean returnValue = false;
-		Integer getIdCount = memberDAO.idCheck(userId);
+		Integer getIdCount = memberService.idCheckService(userId);
 		if(getIdCount == 1) {
 			returnValue = true;
 		}
@@ -83,7 +79,7 @@ public class MemberController {
 	@ResponseBody
 	public Boolean emailCheck(@RequestParam String emailId) {
 		boolean returnValue = false;
-		Integer getEmailCount = memberDAO.emailCheck(emailId);
+		Integer getEmailCount = memberService.emailCheckService(emailId);
 		if(getEmailCount == 1) {
 			returnValue = true;
 		}
