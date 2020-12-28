1b3ab75a6f2be7edaf2b9d74ada44a02b43acfd9
KimJeongho <kimjeongho@archive-asia.co.jp>
2020-12-28 21:56:51 +0900

    댓글에 대한 서비스 수정

--- src/main/java/com/spring/archive/service/CommentService.java
+++ src/main/java/com/spring/archive/service/CommentService.java
@@ -1,5 +1,7 @@
 package com.spring.archive.service;
 
+import java.util.List;
+
 import com.spring.archive.domain.CommentDTO;
 
 public interface CommentService {
@@ -7,5 +9,7 @@ public interface CommentService {
 	void commentWritingService(CommentDTO comment);
 	CommentDTO selectCommentService(Integer cno);
 	void deleteCommentService(Integer cno);
+	Integer getCommentCountService(Integer no);
+	List<CommentDTO> getCommentListService(Integer no);
 	
 }
